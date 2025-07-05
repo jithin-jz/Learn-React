35e9c6144e9afa1a3fb74009fa59e89808980eba
jithin <jithinjzx@gmail.com>
2025-07-05 12:37:16 +0530

    r

---
+++ Conditional Rendering/Rendering/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Greet from './greet'
+const App = () => {
+  return (
+    <div>
+      <Greet isLoged={false} username = 'jithin'/>
+    </div>
+  )
+}
+
+export default App
