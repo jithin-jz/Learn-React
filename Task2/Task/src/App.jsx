ea747d31cf22e1c06e7ab80894f3a1e70c081833
jithin <jithinjzx@gmail.com>
2025-07-07 15:54:56 +0530

    r

---
+++ Task2/Task/src/App.jsx
@@ -0,0 +1,14 @@
+import React, { useState } from 'react'
+
+const App = () => {
+  const [quot,setQuote] = useState(null)
+  
+
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default App
