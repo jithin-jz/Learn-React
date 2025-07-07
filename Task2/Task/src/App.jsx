c1cad236cf819edfb6c587c5ab06c077d0990ea8
jithin <jithinjzx@gmail.com>
2025-07-07 16:07:18 +0530

    r

--- Task2/Task/src/App.jsx
+++ Task2/Task/src/App.jsx
@@ -1,12 +1,21 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 const App = () => {
-  const [quot,setQuote] = useState(null)
-  
+  const [quotes,setQuotes] = useState([]);
 
+  useEffect(()=>{
+    fetch('https://dummyjson.com/quotes')
+    .then(resp =>resp.json())
+    .then(data=>{
+      setQuotes(data.quotes.slice(0,10));
+    })
+    .catch(e => console.log('error:',e))
+  },[]);
   return (
     <div>
-      
+      {quotes.map((quote) => (
+        <h1 key={quote.id}>{quote.quote}</h1>
+      ))}
     </div>
   )
 }
