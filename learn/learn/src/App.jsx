c31ca0556d077b6bd45b12bcfdf22bcf7474eb27
jithin <jithinjzx@gmail.com>
2025-07-08 12:02:15 +0530

    r

--- learn/learn/src/App.jsx
+++ learn/learn/src/App.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const App = () => {
+  const [users,setUsers] = useState([])
+
+  useEffect(()=>{
+    async function fetchUser() {
+      const api = await fetch('https://jsonplaceholder.typicode.com/users')
+      const data = await api.json()
+      setUsers(data)
+    }
+    fetchUser();
+  },[]);
   return (
     <div>
-      
+      {users.map(m=> <p>{m.name}</p>)}
     </div>
   )
 }
