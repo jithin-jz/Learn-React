78ae8b3154653343ce6624285cf2dfeec62f901e
jithin <jithinjzx@gmail.com>
2025-07-10 09:33:19 +0530

    r

--- Conditional Rendering/Rendering/src/Greet.jsx
+++ Conditional Rendering/Rendering/src/Greet.jsx
@@ -1,26 +1,44 @@
-import React from 'react'
+// import React from 'react'
 
-const greet = (props) => {
+// const greet = (props) => {
 
-    // if(props.isLoged){
-    //     return <h2>Welcome {props.username}</h2>
-    //   }
-    //     return <h2>Please Login</h2>
-    return (props.isLoged ? <h2 className='welcome'>Welcome {props.username}</h2>: <h2 className='login'>Please Login</h2>)
+//     // if(props.isLoged){
+//     //     return <h2>Welcome {props.username}</h2>
+//     //   }
+//     //     return <h2>Please Login</h2>
+//     return (props.isLoged ? <h2 className='welcome'>Welcome {props.username}</h2>: <h2 className='login'>Please Login</h2>)
+// }
+
+
+
+
+// // function App() {
+// //     const isLoggedIn = true;
+  
+// //     if (isLoggedIn) {
+// //       return <h1>Welcome back!</h1>;
+// //     } else {
+// //       return <h1>Please log in.</h1>;
+// //     }
+// //   }
+  
+
+// export default greet
+
+import React, { useState } from 'react'
+
+const App = () => {
+  const [isLoged,setLog] = useState(false);
+  return (
+    <div>
+      {isLoged?(
+        <h2>welcome back</h2>
+      ):(
+        <h2>Please Log In</h2>
+      )}
+      <button onClick={()=>setLog(!isLoged)}>click</button>
+    </div>
+  )
 }
 
-
-
-
-// function App() {
-//     const isLoggedIn = true;
-  
-//     if (isLoggedIn) {
-//       return <h1>Welcome back!</h1>;
-//     } else {
-//       return <h1>Please log in.</h1>;
-//     }
-//   }
-  
-
-export default greet
+export default App
--- learn/learn/src/App.jsx
+++ learn/learn/src/App.jsx
@@ -1,77 +1,17 @@
-// import React from 'react'
+import React, { useState } from 'react'
 
-// const App = () => {
-//   const handleClick = () => {
-//     alert('subscriberd')
-//   }
-//   return (
-//     <div>
-//       <button onClick={handleClick}>click here</button>
-//     </div>
-//   )
-// }
+const App = () => {
+  const [isLoged,setLog] = useState(false)
+  return (
+    <div>
+      {isLoged?(
+        <p>Welocme back</p>
+      ):(
+        <p>please login</p>
+      )}
+      <button onClick={()=>setLog(!isLoged)}>click</button>
+    </div>
+  )
+}
 
-// export default App
-
-// import React, { useState } from 'react'
-
-// const App = () => {
-//   const[value,setValue] = useState('')
-
-//   const handleChanges=(e)=>{
-//     setValue(e.target.value)
-//   }
-//   return (
-//     <div>
-//       <input type="text" name="" id="" onChange={handleChanges} />
-//       <p>Text:{value}</p>
-//     </div>
-//   )
-// }
-
-// export default App
-
-// import React, { useState } from 'react'
-
-// const App = () => {
-//     const[count,setCount]=useState(0)
-
-//     const max = () =>{
-//         setCount(p=>p+1)
-//     }
-//     const min = () =>{
-//         setCount(p=>p-1)
-//     }
-//     const reset = () =>{
-//         setCount(0)
-//     }
-//   return (
-//     <div>
-//       <p>{count}</p>
-//       <button onClick={max}>+</button>
-//       <button onClick={min}>-</button>
-//       <button onClick={reset}>reset</button>
-//     </div>
-//   )
-// }
-
-// export default App
-
-
-// import React, { useRef } from 'react';
-
-// function App() {
-//   const inputRef = useRef(null);
-
-//   const focusInput = () => {
-//     inputRef.current.focus(); // Direct DOM access
-//   };
-
-//   return (
-//     <div>
-//       <input ref={inputRef} type="text" />
-//       <button onClick={focusInput}>Focus Input</button>
-//     </div>
-//   );
-// }
-// export default App
+export default App
