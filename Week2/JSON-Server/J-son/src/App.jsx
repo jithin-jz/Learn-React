cf25419ddd491ac7bd4aab4c36716586274078ef
jithin <jithinjzx@gmail.com>
2025-07-11 15:18:14 +0530

    r

--- Week2/JSON-Server/J-son/src/App.jsx
+++ Week2/JSON-Server/J-son/src/App.jsx
@@ -1,43 +1,46 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
 
 const App = () => {
-  const [users,setUsers] = useState([]);
+  const [users, setUsers] = useState([]);
 
   const fetchUsers = async () => {
     const response = await fetch('http://localhost:3000/users');
     const data = await response.json();
     setUsers(data);
-  }
-
-  const addUser = async () =>{
-    await fetch('http://localhost:3000/users', {
-      method:'POST',
-      headers: {
-        'content-Type':'application/json',
-      },
-      body:JSON.stringify({name:'Nithin',email:'nithin@gmail.com'}),
-    });
   };
 
-  useEffect(()=>{
+  const addUser = async () => {
+    await fetch('http://localhost:3000/users', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ name: 'Nithin', email: 'nithin@gmail.com' }),
+    });
+
+    // Refresh user list after adding
     fetchUsers();
-  },[]);
+  };
+
+  useEffect(() => {
+    fetchUsers();
+  }, []);
 
   return (
     <div>
       <h1>User List</h1>
 
+      <button onClick={addUser}>Add New User</button>
+
       <ul>
-        {users.map((u)=>(
-          <li key={u.key}>
-            {u.id} :
-            {u.name}
-            {u.email}
+        {users.map((u) => (
+          <li key={u.id}>
+            {u.id}: {u.name} - {u.email}
           </li>
         ))}
       </ul>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
