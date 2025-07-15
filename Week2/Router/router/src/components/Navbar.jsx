65ab049149ae51964dcd4cf58cf9e66d10edd23a
jithin <jithinjzx@gmail.com>
2025-07-15 11:23:29 +0530

    r

--- Week2/Router/router/src/components/Navbar.jsx
+++ Week2/Router/router/src/components/Navbar.jsx
@@ -1,16 +1,17 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   return (
     <div className="navbar">
       <ul className="nav-links">
-        <li><a href="#">Home</a></li>
-        <li><a href="#">About</a></li>
-        <li><a href="#">Contact</a></li>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/about">About</Link></li>
+        <li><Link to="/contact">Contact</Link></li>
       </ul>
       <button className="login-btn">Login</button>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
